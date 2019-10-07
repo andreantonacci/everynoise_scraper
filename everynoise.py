b38d1f802a9217971e2ef8364eaa1a4ec10b7529
Hannes Datta <h.datta@tilburguniversity.edu>
2019-10-07 11:45:27 +0200

    save raw html files to archive dir; attempt to time-stamp date of everynoise

--- everynoise.py
+++ everynoise.py
@@ -2,12 +2,24 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 import json
 import io
+import time
+from datetime import datetime
+import os
 
+directory = 'html_dumbs'
+try:
+    os.stat(directory)
+except:
+    os.mkdir(directory)    
+    
+
+run_unix = int(time.time())
+run_date = datetime.now().strftime("%Y%m%d")
 
 class EveryNoiseSpider(scrapy.Spider):
     name = "releases"
     start_urls = ['http://everynoise.com/new_releases_by_genre.cgi?region=']
-
+    
     def parse(self, response):
         for region in response.xpath('//select[@name="region"]/option'):
             # reconstruct the url using the country code parameter obtained from the "region" drop-down
@@ -16,6 +28,12 @@ class EveryNoiseSpider(scrapy.Spider):
             yield scrapy.Request(final_url, callback=self.parse_page)
 
     def parse_page(self, response):
+        countryCode = response.request.url[55:57]
+        #every_date=response.css(' body > form > div.title > select:nth-child(4) > option:nth-child(1)')
+       
+        with open(directory+'/page_'+run_date+'_'+countryCode+'.html', 'wb') as backup_file:
+            backup_file.write(response.body)
+        
         for albumrow in response.css('div.albumrow'):
             # if a:nth-child(3) contains no text, then the album name is in the child i
             if albumrow.css('a:nth-child(3)::text').get() is None:
@@ -23,8 +41,7 @@ class EveryNoiseSpider(scrapy.Spider):
             else:
                 albumName = albumrow.css('a:nth-child(3)::text').get()
             # directly obtain the country code from the region parameter in the current url
-            countryCode = response.request.url[55:57]
-
+            
             yield {
                 'countryCode': countryCode,
                 'trackId': albumrow.css('span.play::attr(trackid)').get(),
@@ -33,8 +50,10 @@ class EveryNoiseSpider(scrapy.Spider):
                 'artistName': albumrow.css('a > b::text').get(),
                 'albumId': albumrow.css('a::attr(href)').extract()[1],
                 'albumName': albumName,
+                'scrape_unix': run_unix,
+                'scrape_date': run_date,
+                'everynoise_date': 'PUT DATE HERE, e.g., FROM LISTBOX; see every_date variable above',
             }
-
 
 # list to collect all items
 items = []
@@ -59,7 +78,7 @@ process.crawl(EveryNoiseSpider)
 process.start()  # the script will block here until the crawling is finished
 
 # write output file
-with io.open("output.json", "w", encoding="UTF-8") as json_output:
+with io.open("everynoise_newreleases_"+run_date+".json", "w", encoding="UTF-8") as json_output:
     for item in items:  # loop through objects to add new lines between them
         json.dump(item, json_output, ensure_ascii=False)
         json_output.write("\n")  # add new line for the next object
