016f4184d427fc6f21eff3b6fe0b643ffec8dde7
Hannes Datta <hdatta@datta-research.campus.uvt.nl>
2019-12-10 09:18:16 +0100

    add monitoring script for s3; dumb log of crontab entries

---
+++ monitor_s3.py
@@ -0,0 +1,21 @@
+import boto3
+
+bucket_name = "uvt-streaming-data"
+bucket_dir = 'everynoise/worldbrowser/'
+
+s3 = boto3.resource('s3')
+my_bucket = s3.Bucket(bucket_name)
+
+obj=['<!doctype html>\n<html>\n<head>\n<title>Content of S3 directory</title>\n</head>\n<body>\n<p>']
+
+files = []
+for object_summary in my_bucket.objects.filter(Prefix=bucket_dir):
+    if 'html_dumbs' not in object_summary.key:
+        #obj.append('<br>'+object_summary.key)
+        files.append(object_summary.key)
+files=files[::-1]
+obj.append('<br>'.join(files))
+obj.append('</p>\n</body>\n</html>')
+
+
+boto3.client('s3').put_object(Key='s3.html', Body=''.join(obj), ContentType='text/html', Bucket = 'uvt-public')
