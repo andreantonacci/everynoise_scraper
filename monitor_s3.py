a1efce1c57dc93b8ada8a46aae23c5819d6e4feb
Hannes Datta <hdatta@datta-research.campus.uvt.nl>
2019-12-10 09:22:20 +0100

    add new releases dir to monitor

--- monitor_s3.py
+++ monitor_s3.py
@@ -1,7 +1,7 @@
 import boto3
 
 bucket_name = "uvt-streaming-data"
-bucket_dir = 'everynoise/worldbrowser/'
+bucket_dir = 'everynoise/'
 
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket(bucket_name)
